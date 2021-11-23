59ae3b211bc0f884d961eb73a07ab075e8f189e5
SiriusCM <963937371@qq.com>
2021-11-23 17:25:13 +0800

    整理代码

--- hot/src/main/java/org/sirius/hotfix/hot/Main.java
+++ hot/src/main/java/org/sirius/hotfix/hot/Main.java
@@ -1,11 +1,9 @@
 package org.sirius.hotfix.hot;
 
-import com.sun.tools.attach.AgentInitializationException;
-import com.sun.tools.attach.AgentLoadException;
-import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
 
-import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
 /**
  * @date 2021/11/23
@@ -13,8 +11,36 @@ import java.io.IOException;
  **/
 public class Main {
 
-	public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
-		VirtualMachine vm = VirtualMachine.attach(args[0]);
-		vm.loadAgent(args[1], args[2]);
+	public static void main(String[] args) {
+		System.out.println("----------开始热更代码----------");
+		if (args[0].matches("^[0-9]*$")) {
+			String pid = null;
+			String name = null;
+			try {
+				VirtualMachine virtualMachine = VirtualMachine.attach(args[0]);
+				pid = virtualMachine.id();
+				name = virtualMachine.provider().name();
+				virtualMachine.loadAgent(args[1], args[2]);
+				System.out.println("success:" + name + "@" + pid);
+			} catch (Exception var7) {
+				System.out.println("fail:" + name + "@" + pid);
+			}
+		} else {
+			String pid = String.valueOf(ManagementFactory.getRuntimeMXBean().getPid());
+			for (VirtualMachineDescriptor descriptor : VirtualMachine.list()) {
+				if (!pid.equals(descriptor.id())) {
+					String name = descriptor.displayName();
+					if (name.contains(args[0])) {
+						try {
+							VirtualMachine.attach(descriptor).loadAgent(args[1], args[2]);
+							System.out.println("success:" + name + "@" + descriptor.id());
+						} catch (Exception var6) {
+							System.out.println("fail:" + name + "@" + descriptor.id());
+						}
+					}
+				}
+			}
+		}
+		System.out.println("----------结束热更代码----------");
 	}
 }
